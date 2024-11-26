96eda3c00869420999d427419301616c0cd8b23d
Namorzyny <namorzyny@gmail.com>
2024-11-26 06:03:22 +0000

    Update to version: 3.3.0

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+import {resolve} from 'path';
+
+export const DOWNLOAD_URL = 'https://www.nerdfonts.com/font-downloads';
+export const REPO_PATH = `${resolve(__dirname)}/..`;
+export const FONT_LIST = `${REPO_PATH}/fonts.json`;
